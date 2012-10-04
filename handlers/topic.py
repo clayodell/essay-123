30030a404c81de97e449b6cc8c2b25ce0faa9b68
Faizan Ali <sh.faizan.ali@hotmail.com>
2012-10-04 16:48:25 +0500

    completed add topic handler
    Signed-off-by: Faizan Ali <sh.faizan.ali@hotmail.com>

--- handlers/topic.py
+++ handlers/topic.py
@@ -5,12 +5,13 @@ class addTopic(webapp.RequestHandler):
     def post(self):
         title = self.request.get("title")
         descrtiption = self.request.get("description")
-        accessType = self.request.get("access_type")
+        accessType = int(self.request.get("access_type"))
         mytopic = Topic()
         mytopic.title = title
         mytopic.description = descrtiption
-        mytopic.access_type = 1
+        mytopic.access_type = accessType
         mytopic.owner = "sh.faizan.ali@hotmail.com"
         mytopic.is_deleted = False
         mytopic.put()
-        self.redirect("/")
+        self.redirect("/topics")
+        
