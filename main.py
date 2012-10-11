9d2674ccc8fcfdab76b453ee0702026f5174989e
Faizan Ali <sh.faizan.ali@hotmail.com>
2012-10-11 11:28:31 +0500

    a

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from handlers import index
 from handlers import topic
-from handlers import user
         
 application = webapp.WSGIApplication([
     ('/',index.IndexHandler),
@@ -27,7 +26,6 @@ application = webapp.WSGIApplication([
     ('/create-topic',index.TopicHandler),
     ('/do-add_topic',topic.addTopic),
     ('/topics',topic.viewPublicTopics),
-    ('/login',user.LoginPageHandler),
 ], debug=True)
 
 def main():
